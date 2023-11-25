354fd01c77b7dd1d0ef3f26dc418597c889e2e67
leejunghyeok <wndgur76@naver.com>
2023-11-25 16:22:04 +0900

    홈 가면 랜덤페이지로

--- app/page.js
+++ app/page.js
@@ -3,18 +3,12 @@ import { getRandomURL } from "@/util/controller";
 import { useEffect } from "react"
 
 export default function Home() {
-    // useEffect(() => {
-    //     window.location.href = '/hub/0'
-    // }, []);
+    useEffect(() => {
+        window.location.href = '/hub/' + encodeURI(getRandomURL());
+    }, []);
     return (
         <div style={{width:"100%", height:"100%", display:"flex", justifyContent:"center", alignItems:"center"}}>
             <h1>404</h1>
-            <button onClick={()=>{
-                getRandomURL().then((data)=>{
-                    console.log(data);
-                })
-                
-            }}>distinct</button>
         </div>
     )
 }
