bcbf2a3d0794df8571c9cc7490e20992eb84cb34
leejunghyeok <wndgur76@naver.com>
2023-11-25 16:25:15 +0900

    Refactor window location.href in Home component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,13 @@ import { useEffect } from "react"
 
 export default function Home() {
     useEffect(() => {
-        window.location.href = '/hub/' + encodeURI(getRandomURL());
+        getRandomURL().then(data=>{
+            let url = data.query.url;
+            window.location.href = `/hub/${url}`;
+        }).catch(err=>{
+            console.log(err);
+            window.location.href = `/hub/0`;
+        });
     }, []);
     return (
         <div style={{width:"100%", height:"100%", display:"flex", justifyContent:"center", alignItems:"center"}}>
