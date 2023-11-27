353e877389d3f8b97e4a291c1046aedd68932cc2
joonmoogo <junemuk10@gmail.com>
2023-11-27 10:43:02 +0900

    add config

--- util/socket-client.js
+++ util/socket-client.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
-// var socket = io('https://port-0-server-for-tableorder-7lk2blopce1o7.sel5.cloudtype.app/');
-var socket = io('http://localhost:8090/');
+import config from './config';
+var socket = io(config.socketServer);
+// var socket = io('http://localhost:8090/');
 
 
 export default socket;
