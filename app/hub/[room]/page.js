a908872e5bdcb2a19bfb2559668d85074b859cc3
leejunghyeok <wndgur76@naver.com>
2023-11-27 12:35:05 +0900

    share styles

--- app/hub/[room]/page.js
+++ app/hub/[room]/page.js
@@ -79,13 +79,13 @@ export default function Rooms() {
 
                 <div className={styles.shares}>
                     <div className={styles.shareWrapper + ' ' + styles.share}>
-                        <img id='kakaotalk-sharing-btn' src="/images/shares/kakao_round.png" alt="카카오톡 공유 보내기 버튼" width="42px" height="42px" />
-                    </div>
-                    <div className={styles.shareWrapper + ' ' + styles.share}>
                         <img onClick={shareBtnHandler} src="/images/shares/share2.svg" alt="공유 보내기 버튼" width="29px" height="29px" />
                     </div>
                     <div className={styles.shareWrapper + ' ' + styles.share}>
                         <img onClick={copyBtnHandler} src="/images/shares/copy.svg" alt="공유 보내기 버튼" width="32px" height="32px" />
+                    </div>
+                    <div className={styles.shareWrapper + ' ' + styles.share} style={{boxShadow:"none"}}>
+                        <img id='kakaotalk-sharing-btn' src="/images/shares/kakao_round.png" alt="카카오톡 공유 보내기 버튼" width="42px" height="42px" />
                     </div>
                 </div>
 
